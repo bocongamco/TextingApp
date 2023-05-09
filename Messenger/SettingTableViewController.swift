09ed70f63724dc90ba2468518a55be7545a3c923
Nhat Tien Phat Cao <ncaovncountry@gmail.com>
2023-05-09 18:55:24 +1000

    Remove all empty cell in setting tab Modified bit about UI

---
+++ Messenger/SettingTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTableViewController.swift
+//  Messenger
+//
+//  Created by Phan Thi Quynh on 09/05/2023.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//      Remove all empty cell
+        tableView.tableFooterView = UIView()
+        
+    }
+
+    // MARK: - Table view data source
+
+    
+
+}
